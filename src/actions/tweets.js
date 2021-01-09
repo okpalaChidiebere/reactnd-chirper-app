a06d01159d786257fabb49ef6aee1f3fd7c9ebd5
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-09 17:19:09 -0600

    Liking a Tweet: Redux Action
    At this point, we had the UI for our timeline, but if you click on any of the hearts to like the tweet nothing will happen because we have not implement any of that functionality yet.
    
    - Remember, in the API, we have a function where we can save that specific like.
    - We imported that saveLikeToggle function to /actions/tweets.js
    - We described this specific event and created our action creator
    - The we created an asynchronous action creator called handleToogleLike that will be responsible for invoking our saveLikeToggle function and this async function will be the function that we will export. Redux thunks to the rescue!

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,9 +1,33 @@
+import { saveLikeToggle } from '../utils/api'
+
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_LIKE = 'TOGGLE_LIKE'
 
 //receive tweets action creator
 export function receiveTweets (tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets,
+  }
+}
+
+const toggleLike = ({ id, authedUser, hasLiked }) => ({
+  type: TOGGLE_LIKE,
+  id,
+  authedUser,
+  hasLiked
+})
+
+export const handleToggleLike = (info) => async (dispatch) => {
+
+  try {
+    /*We are using Optimistic Updates in this operation */
+    dispatch(toggleLike(info))
+    await saveLikeToggle(info)
+
+  }catch(e){
+    console.warn('Error in handleToggleTweet: ', e)
+    dispatch(toggleLike(info)) //we dispatch ogleLike one more time so that it will reset back to what it initially was
+    alert('There was an error liking the tweet. Try again.')
   }
 }
