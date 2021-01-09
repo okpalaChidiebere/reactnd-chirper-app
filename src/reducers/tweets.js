63b5b40438318b291c74142de0e5b8ea2144c0dc
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-09 17:34:49 -0600

    Liking a Tweet - Redux Reducer
    When liking a tweet (or unliking a tweet), the state for that specific tweet needs to change - either the tweet's like property (which, if you remember, is an array and will contain the names of the users that have liked the tweet) will need to change to include the user that clicked it (if they're liking the tweet) or the user's name will need to be removed from the array (if they're unliking the tweet).
    
    So we need to update the reducer to handle these changes.
    
    - Inside the reducer/tweets.js, we imported the TOGGLE_LIKE action type
    - We added a new case to our reducer for if the case is TOOGLE_LIKE, how do we want to change the shape of the tweets slice of our state tree
    
    Remember in your reducers you will use the spread operator, .filert(), .concat(), .map(), object.assign(), .sort() a lot. Basically any functionality that does not mutate data. This means, that you get thesame result even as long a you invoke them with thesame inputs

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_LIKE } from '../actions/tweets'
 
 export default function tweets (state = {}, action) {
   switch(action.type) {
@@ -6,6 +6,17 @@ export default function tweets (state = {}, action) {
       return {
         ...state,
         ...action.tweets
+      }
+    case TOGGLE_LIKE :
+      return {
+        ...state, //we dont want to mutate the store, so we spread the previous tweets on object
+        [action.id]: {
+          ...state[action.id],
+          //we add a user to the likes array or remove a user from the likes array of they have already liked it
+          likes: action.hasLiked === true
+            ? state[action.id].likes.filter((uid) => uid !== action.authedUser) //remove
+            : state[action.id].likes.concat([action.authedUser]) //add
+        }
       }
     default :
       return state
