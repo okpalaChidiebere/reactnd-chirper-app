4d7274cc42f559fbc223a200f5b6df0fa5506ca4
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-01-09 19:21:37 -0600

    New Tweet Component : Redux reducer
    We wrote the logc that updates the tweets slice of store when the usr adda a new tweet
    
    SIDE NOTE:
    Make sure that whenever an arrow function has curly braces, you’re using a return statement, if you want to return something.
    Given this array, const nums = [1,2,3]; Which statements produce the following output [3,3,3] ?
    You can do any of the following below
    nums.map(num => nums.length)
    nums.map(num => { return nums.length })
    
    However, We did not use the map function to return a new state tree, but sure can implement this using .map function.
    Check this code
    https://github.com/okpalaChidiebere/folder-structure-react-redux/blob/master/src/reducers/todos.js

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TWEETS, TOGGLE_LIKE } from '../actions/tweets'
+import { RECEIVE_TWEETS, TOGGLE_LIKE, ADD_TWEET } from '../actions/tweets'
 
 export default function tweets (state = {}, action) {
   switch(action.type) {
@@ -17,6 +17,29 @@ export default function tweets (state = {}, action) {
             ? state[action.id].likes.filter((uid) => uid !== action.authedUser) //remove
             : state[action.id].likes.concat([action.authedUser]) //add
         }
+      }
+    case ADD_TWEET :
+      const { tweet } = action
+
+      let replyingTo = {}
+      /*We want to go to the array property(replies) if we are replying to a tweet and add the 
+      tweetId there as well */
+      if (tweet.replyingTo !== null) {
+        /*At this point an already existing tweet needs to be modified */
+
+        replyingTo = {
+          [tweet.replyingTo]: {
+            ...state[tweet.replyingTo],
+            replies: state[tweet.replyTo].replies.concat([tweet.id]) //we go the reply array of the tweet we are replying to and add the id of this new tweet we posted
+          }
+        }
+      }
+
+      
+      return {
+        ...state,
+        [action.tweet.id]: action.tweet, //adding our new tweet to our new tweet state to the tweets slice state
+        ...replyingTo, //spread the modified existing tweet across our new tweets state
       }
     default :
       return state
